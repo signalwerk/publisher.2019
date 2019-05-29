be3545e60fecbdb1072670d0eb940e08a29503e4
Stefan Huber <sh@signalwerk.ch>
2019-05-29 22:16:49 +0200

    MOD: update build system

--- build.sh
+++
@@ -1,7 +0,0 @@
-lead='<!-- page content start-->'
-tail='<!-- page content end -->'
-
-## watch -n20 sh build.sh
-npm run md
-npm run build
-awk 'NR==FNR{new = new $0 ORS; next} /'"$tail"'/{f=0} !f{print} /'"$lead"'/{printf "%s",new; f=1}' ./text/Frontend-Frameworks/index.html ./dist/index.html > ./dist/frontend-frameworks.html
