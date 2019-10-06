b92321d4f9d454b4dbd8f6682ba2f2b594dd96b4
Stefan Huber <sh@signalwerk.ch>
2019-10-06 14:28:30 +0200

    ADD: webtypo-space

--- ci/build.sh
+++ ci/build.sh
@@ -7,8 +7,10 @@ tail='<!-- page content end -->'
 ## watch -n20 sh build.sh
 npm run mdA
 npm run mdB
+npm run mdC
 npm run build
 awk 'NR==FNR{new = new $0 ORS; next} /'"$tail"'/{f=0} !f{print} /'"$lead"'/{printf "%s",new; f=1}' ./text/frontend-frameworks/index.html ./dist/index.html > ./dist/frontend-frameworks.html
 awk 'NR==FNR{new = new $0 ORS; next} /'"$tail"'/{f=0} !f{print} /'"$lead"'/{printf "%s",new; f=1}' ./text/design-systems/index.html ./dist/index.html > ./dist/design-systems.html
+awk 'NR==FNR{new = new $0 ORS; next} /'"$tail"'/{f=0} !f{print} /'"$lead"'/{printf "%s",new; f=1}' ./text/webtypo-space/index.html ./dist/index.html > ./dist/webtypo-space.html
 cp ./text/frontend-frameworks/graphics_cmyk.pdf ./dist/frontend-frameworks_graphics_cmyk.pdf
 cp ./text/design-systems/graphics_cmyk.pdf ./dist/design-systems_graphics_cmyk.pdf
---
+++ text/webtypo-space/index.md
@@ -0,0 +1,26 @@
+## Publisher 5 · 2019
+Erscheinungsdatum · 14. 10. 2019
+
+<hr class="thick"  />
+
+## Lizenzen
+Dieser Text ist von Stefan Huber unter der Lizenz [CC-BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) veröffentlicht. [Publisher](https://publisher.ch/) erhält das Recht die Inhalte ohne Copyleft (Share Alike) zu publizieren.
+
+<!--
+<hr class="thick"  />
+[CMYK Bilder für Print (pdf)](./frontend-frameworks_graphics_cmyk.pdf)
+ -->
+
+<hr class="thick"  />
+
+
+# Mikrotypografie im Web – Leerzeichen
+
+
+<hr class="thick"  />
+
+### Autor
+Stefan Huber unterrichtet an der [Schule für Gestaltung Zürich](https://sfgz.ch/) im Lehrgang HF Interaction Design und ist als Web-Entwickler tätig.
+
+
+<hr class="thick"  />
